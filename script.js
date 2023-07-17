b902c01d3f5cd0a0c7b348e55420956da13ba4a8
harsh05773 <119809198+harsh05773@users.noreply.github.com>
2023-07-17 23:27:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Frontend Developer",
+        "Coder",
+        "Software Developer"
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
